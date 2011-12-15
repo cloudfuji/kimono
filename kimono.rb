dd5bf336ec1a68af103eafa2d1ccd076f746f489
Akash Manohar J <akash@akash.im>
2011-12-15 16:11:38 +0530

    Removes commented out branch option for devuse_bushido_authenticatable
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- kimono.rb
+++ kimono.rb
@@ -62,7 +62,7 @@ say_recipe "Devise"
 @configs[@current_recipe] = config
 
 gem "devise"
-gem "devise_bushido_authenticatable", :git =>"https://github.com/Bushido/devise_cas_authenticatable.git"#, :branch => "nbudin_updates"
+gem "devise_bushido_authenticatable", :git =>"https://github.com/Bushido/devise_cas_authenticatable.git"
 
 after_bundler do
   generate("devise:install")
