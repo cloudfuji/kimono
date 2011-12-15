d8f5399e69276f603d9ae44fd7ed81183c25ac29
Akash Manohar J <akash@akash.im>
2011-12-15 13:46:53 +0530

    Adds more baby-sitting by injecting a bushido_extra_attributes method into the model and changes generated migration
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- kimono.rb
+++ kimono.rb
@@ -1,13 +1,15 @@
 gem "bushido", :git=>"https://github.com/Bushido/bushidogem.git"
 gem "devise_bushido_authenticatable", :git =>"https://github.com/Bushido/devise_cas_authenticatable.git"
 
-gem "rspec-rails",     :group => "development"
-gem "awesome_print",   :group => "development"
-gem "tane",            :group => "development", :git => "https://github.com/Bushido/tane.git"
+gem "rspec-rails",        :group => "development"
+gem "factory_girl_rails", :group => :development
+gem "awesome_print",      :group => "development"
+gem "tane",               :group => "development", :git => "https://github.com/Bushido/tane.git"
 
 
+# lib/bushido/hooks/user_hooks.rb
 lib("bushido/hooks/user_hooks.rb") do
-<<EOF
+<<-EOF
 class BushidoUserHooks < Bushido::EventObserver
   def user_added
     user.create(:email  => params['data']['email'],
@@ -22,8 +24,10 @@ end
 EOF
 end
 
+
+# lib/bushido/hooks/app_hooks.rb
 lib('bushido/hooks/app_hooks.rb') do
-<<EOF
+<<-EOF
 class BushidoAppHooks < Bushido::EventObserver
   def app_claimed
     User.find(1).update_attributes(:email  => params['data']['email'],
@@ -33,8 +37,10 @@ end
 EOF
 end
 
+
+# lib/bushido/bushido_mail_routes.rb
 lib("bushido/bushido_mail_routes.rb") do
-<<EOF
+<<-EOF
 # Mail routes
 ::Bushido::Mailroute.map do |m|
 
@@ -46,8 +52,10 @@ end
 EOF
 end
 
+
+# lib/bushido/hooks/email_hooks.rb
 lib("bushido/hooks/email_hooks.rb") do
-<<EOF
+<<-EOF
 class BushidoEmailHooks < Bushido::EventObserver
 
   def mail_simple
@@ -60,20 +68,26 @@ end
 EOF
 end
 
+
+# config/initializers/bushido_hooks.rb
 create_file("config/initializers/bushido_hooks.rb") do
-<<EOF
+<<-EOF
 Dir["\#{Dir.pwd}/lib/bushido/**/*.rb"].each { |file| require file }
 EOF
 end
 
+
+# config/initializers/bushido_mail_routes.rb
 create_file("config/initializers/bushido_mail_routes.rb") do
-<<EOF
+<<-EOF
 require './lib/bushido/bushido_mail_routes.rb'
 EOF
 end
 
+
+# config/routes.rb
 prepend_to_file("config/routes.rb") do
-<<EOF
+<<-EOF
 begin
   Rails.application.routes.draw do
     bushido_routes
@@ -85,7 +99,41 @@ end
 EOF
 end
 
-run "bundle install"
 
-generate('devise:install')
-generate('devise', 'User')
+run("bundle install")
+generate("devise:install")
+generate("devise", "User")
+
+
+Dir["db/migrate/*devise_create_*"].each do |file|
+  # Replace database_authenticatable with bushido_authenticatable in the migration
+  gsub_file file, "database_authenticatable :null => false", "bushido_authenticatable"
+
+  # Replace the following lines to create fields for extra attributes
+  gsub_file file, "t.recoverable",  "t.string :email"
+  gsub_file file, "t.rememberable", "t.string :first_name"
+  gsub_file file, "t.trackable",    "t.string :last_name"
+
+  # replace add_index for reset_password_token with ido_id
+  gsub_file file, "reset_password_token", "ido_id"
+end
+
+inject_into_class "app/models/user.rb", "User" do
+<<-EOF
+  def bushido_extra_attributes(extra_attributes)
+    self.first_name = extra_attributes["first_name"].to_s
+    self.last_name  = extra_attributes["last_name"].to_s
+    self.email      = extra_attributes["email"]
+  end
+EOF
+end
+
+
+gsub_file "app/models/user.rb", ":recoverable, :rememberable, :trackable, :validatable", ""
+gsub_file "app/models/user.rb", ":database_authenticatable", ":bushido_authenticatable"
+gsub_file "app/models/user.rb", ", :registerable,", ""
+
+gsub_file "app/models/user.rb",
+          "attr_accessible :email, :password, :password_confirmation, :remember_me",
+          "attr_accessible :email, :ido_id, :first_name, :last_name"
+
