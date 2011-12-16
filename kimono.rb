003e96202ce2bcad0a9df89a22db01c149cd069a
Akash Manohar J <akash@akash.im>
2011-12-16 22:26:50 +0530

    Uses available generators instead of duplication
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- kimono.rb
+++ kimono.rb
@@ -114,53 +114,11 @@ gem "bushido", :git=>"https://github.com/Bushido/bushidogem.git"
 
 after_bundler do 
   generate("bushido:mail_routes")
-
-  # lib/bushido/hooks/user_hooks.rb
-  lib("bushido/hooks/user_hooks.rb") do
-  <<-EOF
-class BushidoUserHooks < Bushido::EventObserver
-  def user_added
-    user.create(:email  => params['data']['email'],
-      :ido_id => params['data']['ido_id'],
-      :active => true)
-  end
-
-  def user_removed
-    User.find_by_ido_id(params['data']['ido_id']).try(:disable!)
-  end
-end
-  EOF
-  end
-
-
-  # lib/bushido/hooks/app_hooks.rb
-  lib('bushido/hooks/app_hooks.rb') do
-  <<-EOF
-class BushidoAppHooks < Bushido::EventObserver
-  def app_claimed
-    User.find(1).update_attributes(:email  => params['data']['email'],
-      :ido_id => params['data']['ido_id'])
-  end
-end
-  EOF
-  end
-
+  generate("bushido:app_hooks")
+  generate("bushido:user_hooks")
+  generate("bushido:routes")
 end
 
-
-# config/routes.rb
-prepend_to_file("config/routes.rb") do
-<<-EOF
-begin
-  Rails.application.routes.draw do
-    bushido_routes
-  end
-rescue => e
-  puts "Error loading the Bushido routes:"
-  puts "\#{e.inspect}"
-end
-EOF
-end
 
 # >----------------------------------[ Tane ]----------------------------------<
 
